0672f59d97287ab5f8ba2aaf4cc4896d42a1f2c0
Salman <muhammadsalman410@gmail.com>
2023-07-19 22:40:58 +0500

    change mongodb connection

--- app.js
+++ app.js
@@ -52,12 +52,11 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await dbConnect(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
   } catch (error) {
     console.log(error);
   }
 };
 
 start();
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));

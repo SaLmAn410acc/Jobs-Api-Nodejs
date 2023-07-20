6a8464286985a590ce7891f41cd242c46f9dd489
Salman <muhammadsalman410@gmail.com>
2023-07-20 16:15:43 +0500

    add swagger file

--- app.js
+++ app.js
@@ -24,25 +24,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // extra packages
 
-app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  })
-);
+// app.set("trust proxy", 1);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   })
+// );
 
-app.use(helmet());
-app.use(cors());
-app.use(xss());
+// app.use(helmet());
+// app.use(cors());
+// app.use(xss());
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobRouter);
 
 app.get("/", (req, res) => {
-  res.send("Jobs Api home");
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
